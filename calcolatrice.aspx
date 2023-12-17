158ecb7892f8c0df462cb707ec8cade328e5f842
BerasoIT <beraso95@gmail.com>
2023-12-17 17:18:13 +0100

    firstUpload

---
+++ calcolatrice.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="calcolatrice.aspx.cs" Inherits="calcolatrice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Calcolatrice</title>
+    <link href="style.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div>
+            <table>
+
+                <tr>
+                    <td colspan="4">
+                        <asp:TextBox ID="txtDisplay" runat="server" Enabled="False"></asp:TextBox>
+                    </td>
+                </tr>
+
+
+
+                <tr>
+                    <td>
+                        <asp:Button ID="btnCanc" class="bottoneFunzione" runat="server" Text="C" OnClick="btnCanc_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnNegativo" class="bottoneFunzione" runat="server" Text="+/-" OnClick="btnNegativo_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnPercentuale" class="bottoneFunzione" runat="server" Text="%" OnClick="btnPercentuale_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnDivisione" class="bottoneOperatori" runat="server" Text="÷" OnClick="btnDivisione_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Button ID="btn7" class="bottoneNumeri" runat="server" Text="7" OnClick="btn7_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn8" class="bottoneNumeri" runat="server" Text="8" OnClick="btn8_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn9" class="bottoneNumeri" runat="server" Text="9" OnClick="btn9_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnMoltiplicazione" class="bottoneOperatori" runat="server" Text="x" OnClick="btnMoltiplicazione_Click" /></td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Button ID="btn4" class="bottoneNumeri" runat="server" Text="4" OnClick="btn4_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn5" class="bottoneNumeri" runat="server" Text="5" OnClick="btn5_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn6" class="bottoneNumeri" runat="server" Text="6" OnClick="btn6_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnSottrazione" class="bottoneOperatori" runat="server" Text="-" OnClick="btnSottrazione_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Button ID="btn1" class="bottoneNumeri" runat="server" Text="1" OnClick="btn1_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn2" class="bottoneNumeri" runat="server" Text="2" OnClick="btn2_Click" /></td>
+                    <td>
+                        <asp:Button ID="btn3" class="bottoneNumeri" runat="server" Text="3" OnClick="btn3_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnAddizione" class="bottoneOperatori" runat="server" Text="+" OnClick="btnAddizione_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btn0" class="bottoneNumeri" runat="server" Text="0" OnClick="btn0_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnDecimale" class="bottoneNumeri" runat="server" Text="," OnClick="btnDecimale_Click" /></td>
+                    <td>
+                        <asp:Button ID="btnUguale" class="bottoneOperatori" runat="server" Text="=" OnClick="btnUguale_Click" /></td>
+                </tr>
+
+
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ calcolatrice.aspx.cs
@@ -0,0 +1,633 @@
+﻿using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class calcolatrice : System.Web.UI.Page
+{
+
+    //DICHIARAZIONE VARIABILI GLOBALI
+    static string num1 = "";
+    static string num2 = "";
+    static string op = "";
+
+    static float num1f; //variabile float di appoggio a variabile num1 nei casi in cui serve come numero e non come stringa
+    static float num2f; //variabile float di appoggio a variabile num2 nei casi in cui serve come numero e non come stringa
+    static float tot;
+
+    static int flag = 0;
+
+
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    //EVENTI BOTTONI NUMERO
+    protected void btnCanc_Click(object sender, EventArgs e)
+    {
+        num1 = "";
+        num2 = "";
+        op = "";
+        flag = 0;
+
+        txtDisplay.Text = "";
+    }
+
+
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1) { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+        if (op == "")
+        {
+
+            if (txtDisplay.Text == "")
+            {
+                num1 += "0";
+                txtDisplay.Text = num1;
+            }
+            else
+            {
+                if (txtDisplay.Text != "" && txtDisplay.Text != "0")
+                {
+                    num1 += "0";
+                    txtDisplay.Text = num1;
+                }
+            }
+
+
+
+        }
+        else
+        {
+
+            if (txtDisplay.Text != "")
+            {
+                if (txtDisplay.Text != "0")
+                {
+                    num2 += "0";
+                    txtDisplay.Text = num2;
+                }
+            }
+
+
+        }
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+        if (op == "")
+        {
+            num1 += "1";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "1";
+            txtDisplay.Text = num2;
+        }
+    }
+
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "2";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "2";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "3";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "3";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "4";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "4";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "5";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "5";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "6";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "6";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "7";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "7";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "8";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "8";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+
+        if (flag == 1)
+        { //controllo se è in corso un operazione o se è nesessario ricominciare a seguito di un uguale
+
+            num1 = "";
+            num2 = "";
+            op = "";
+            flag = 0;
+            txtDisplay.Text = "";
+        }
+
+
+        if (op == "")
+        {
+            num1 += "9";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            num2 += "9";
+            txtDisplay.Text = num2;
+        }
+    }
+
+    //EVENTI BOTTONI OPERATORE
+
+
+    protected void btnAddizione_Click(object sender, EventArgs e)
+    {
+        if (num1 != "")
+        {
+            if (num2 == "")
+            {
+                op = "+";
+            }
+            else
+            {
+                //CONTROLLO OPERATORE
+                if (op == "+")
+                {
+                    tot = float.Parse(num1) + float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "-")
+                {
+                    tot = float.Parse(num1) - float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "*")
+                {
+                    tot = float.Parse(num1) * float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "/")
+                {
+                    tot = float.Parse(num1) / float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                //FINE CONTROLLO OPERATORE
+
+                num1 = tot.ToString();
+                txtDisplay.Text = num1;
+                num2 = "";
+                op = "+";
+            }
+        }
+    }
+
+    protected void btnSottrazione_Click(object sender, EventArgs e)
+    {
+
+        if (num1 != "")
+        {
+            if (num2 == "")
+            {
+                op = "-";
+            }
+            else
+            {
+                //CONTROLLO OPERATORE
+                if (op == "+")
+                {
+                    tot = float.Parse(num1) + float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "-")
+                {
+                    tot = float.Parse(num1) - float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "*")
+                {
+                    tot = float.Parse(num1) * float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "/")
+                {
+                    tot = float.Parse(num1) / float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                //FINE CONTROLLO OPERATORE
+
+                num1 = tot.ToString();
+                txtDisplay.Text = num1;
+                num2 = "";
+                op = "-";
+            }
+        }
+    }
+
+    protected void btnMoltiplicazione_Click(object sender, EventArgs e)
+    {
+
+        if (num1 != "")
+        {
+            if (num2 == "")
+            {
+                op = "*";
+            }
+            else
+            {
+                //CONTROLLO OPERATORE
+                if (op == "+")
+                {
+                    tot = float.Parse(num1) + float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "-")
+                {
+                    tot = float.Parse(num1) - float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "*")
+                {
+                    tot = float.Parse(num1) * float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "/")
+                {
+                    tot = float.Parse(num1) / float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                //FINE CONTROLLO OPERATORE
+                num1 = tot.ToString();
+                txtDisplay.Text = num1;
+                num2 = "";
+                op = "*";
+            }
+        }
+    }
+
+    protected void btnDivisione_Click(object sender, EventArgs e)
+    {
+        if (num1 != "")
+        {
+            if (num2 == "")
+            {
+                op = "/";
+            }
+            else
+            {
+                //CONTROLLO OPERATORE
+                if (op == "+")
+                {
+                    tot = float.Parse(num1) + float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "-")
+                {
+                    tot = float.Parse(num1) - float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "*")
+                {
+                    tot = float.Parse(num1) * float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                if (op == "/")
+                {
+                    tot = float.Parse(num1) / float.Parse(num2);
+                    txtDisplay.Text = tot.ToString();
+                }
+                //FINE CONTROLLO OPERATORE
+
+                num1 = tot.ToString();
+                txtDisplay.Text = num1;
+                num2 = "";
+                op = "/";
+            }
+        }
+    }
+
+
+    //EVENTI BOTTONE UGUALE
+
+    protected void btnUguale_Click(object sender, EventArgs e)
+    {
+
+
+
+        if (num1 != "")
+        {
+            if (num2 != "")
+            {
+                if (op == "/" && num2 == "0")
+                {
+                    txtDisplay.Text = "Errore";
+                }
+                else
+                {
+                    if (op == "+")
+                    {
+                        tot = float.Parse(num1) + float.Parse(num2);
+                        txtDisplay.Text = tot.ToString();
+                        num1 = tot.ToString();
+                        num2 = "";
+                        op = "";
+                    }
+                    if (op == "-")
+                    {
+                        tot = float.Parse(num1) - float.Parse(num2);
+                        txtDisplay.Text = tot.ToString();
+                        num1 = tot.ToString();
+                        num2 = "";
+                        op = "";
+                    }
+                    if (op == "*")
+                    {
+                        tot = float.Parse(num1) * float.Parse(num2);
+                        txtDisplay.Text = tot.ToString();
+                        num1 = tot.ToString();
+                        num2 = "";
+                        op = "";
+                    }
+                    if (op == "/")
+                    {
+                        tot = float.Parse(num1) / float.Parse(num2);
+                        txtDisplay.Text = tot.ToString();
+                        num1 = tot.ToString();
+                        num2 = "";
+                        op = "";
+                    }
+
+                    flag = 1;
+                }
+            }
+        }
+    }
+
+
+
+    //EVENTI BOTTONE NEGATIVO
+
+    protected void btnNegativo_Click(object sender, EventArgs e)
+    {
+
+        if (txtDisplay.Text != "")
+        {
+            num1f = float.Parse(txtDisplay.Text);
+
+            if (num1f > 0)
+            {
+                txtDisplay.Text = "-" + txtDisplay.Text;
+                num1 = txtDisplay.Text;
+                op = "";
+                num2 = "";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text.Remove(0, 1);
+                num1 = txtDisplay.Text;
+                op = "";
+                num2 = "";
+            }
+        }
+
+    }
+
+    //EVENTI BOTTONE PERCENTUALE
+
+    protected void btnPercentuale_Click(object sender, EventArgs e)
+    {
+        if (txtDisplay.Text != "")
+        {
+
+            if (op == "")
+            {
+                num1f = 0;
+                num1f = float.Parse(num1) / 100;
+                num1 = num1f.ToString();
+                txtDisplay.Text = num1;
+            }
+            else
+            {
+                num2f = 0;
+                num2f = float.Parse(num2) / 100;
+                num2 = num2f.ToString();
+                txtDisplay.Text = num2;
+            }
+        }
+    }
+
+    //EVENTI BOTTONE DECIMALE
+
+
+
+    protected void btnDecimale_Click(object sender, EventArgs e)
+    {
+
+        if (txtDisplay.Text == "")
+        {
+            num1 += "0,";
+            txtDisplay.Text = num1;
+        }
+        else
+        {
+            if (op == "")
+            {
+
+                num1 += ",";
+                txtDisplay.Text = num1;
+            }
+            else
+            {
+                num2 += ",";
+                txtDisplay.Text = num2;
+            }
+        }
+    }
+}
